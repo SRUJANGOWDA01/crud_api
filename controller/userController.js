0403c87817a4e86ef88f22c0a1a85f6c90805c1b
SRUJANGOWDA01 <srujangowda46@gmail.com>
2024-02-23 12:03:24 +0530

    findone & create

--- controller/userController.js
+++ controller/userController.js
@@ -1,3 +1,4 @@
+const User = require('../model/user')
 
 //read all users
 const readAll = function(req,res){
@@ -8,10 +9,28 @@ const readSingle = function(req,res){
     res.status(200).json({ status: true, msg: " read single user"})
 }
 
-//read single user
-const createUser = function (req,res){
-    res.status(201).json({ status: true, msg: "create new user"})
+//create user
+const createUser = async (req,res) => {
+    //reading the incoming data (urlencoded/json)
+    let data = req.body
+
+    //check for existing data
+    //existing email
+    let extUser = await User.findOne({email: data.email})
+    if(extUser)
+         return res.status(400).json({ status: false, msg: `user email ${data.email} already exists`})
+
+    //existing mobile number
+    let extMobile = await User.findOne({mobile: data.mobile})
+    if(extMobile)
+         return res.status(400).json({ status: false, msg: `user mobile ${data.mobilel} already registered`})
+
+         //to create new user data
+    let newUser = await User.create(data) 
+     // final response
+    res.status(201).json({ status: true, msg: " New user created successfuly", newUser})
 }
+
 
 // update user
 const updateUser = function(req,res) {
--- server.js
+++ server.js
@@ -2,11 +2,22 @@ const express = require('express')
 
 //settings to access .env variables
 require('dotenv').config()
+const connectDb = require('./db/connect')
+const cors = require('cors')
 
 const PORT = process.env.PORT
 
 // instance of express
 const app = express()
+
+//body parser middleware
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+// middleware
+// cors => cross origin resource sharing
+app.use(cors())
+
 
 // app.get(route,controller)
 // index route - get request method
@@ -24,5 +35,12 @@ app.all('*', (req,res)=> {
 
 // server listen
 app.listen(PORT,() => {
+    if(process.env.MODE === "development") {
+        // to connect local database
+        connectDb(process.env.MONGO_DEV)
+    } else if(process.env.MODE === "production") {
+        // to connect cloud database
+        connectDb(process.env.MONGO_URL)
+    }
     console.log(`server is running @ http://localhost:${PORT}`)
 })
